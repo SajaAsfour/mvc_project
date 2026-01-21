4be59758f491496be04c83fd25fa295b5ea4a13d
SajaAsfour <sajanazih2004@gmail.com>
2026-01-21 21:10:30 +0200

    publish lec 5

--- KASHOP/Data/ApplicationDbContext.cs
+++ KASHOP/Data/ApplicationDbContext.cs
@@ -7,7 +7,12 @@ namespace KASHOP.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Data Source=.;DataBase=mvcProject;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
+
+            //DataBase for deployment (production)
+            optionsBuilder.UseSqlServer("Server=db38750.public.databaseasp.net; Database=db38750; User Id=db38750; Password=b_2LXa%3#5yT; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True; ");
+
+            //DataBase for development
+            //optionsBuilder.UseSqlServer("Data Source=.;DataBase=mvcProject;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
         }
     }
 }
