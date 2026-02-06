00244be4566af99354b426e1b7d86cea36e6b097
SajaAsfour <sajanazih2004@gmail.com>
2026-02-06 14:04:21 +0200

    View Products in each category in user area

--- KASHOP/Areas/User/Controllers/HomeController.cs
+++ KASHOP/Areas/User/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using KASHOP.Data;
+using KASHOP.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KASHOP.Areas.User.Controllers
 {
@@ -11,6 +13,24 @@ namespace KASHOP.Areas.User.Controllers
         {
             var categories = context.categories.ToList();
             return View(categories);
+        }
+
+        public IActionResult ByCategory(int id)
+        {
+            var products = context.products
+                .Where(p => p.CategoryId == id)
+                .Select(p => new ProductsViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Image = p.Image
+                })
+                .ToList();
+
+            ViewBag.CategoryName = context.categories
+                .FirstOrDefault(c => c.Id == id)?.Name;
+
+            return View(products);
         }
     }
 }
